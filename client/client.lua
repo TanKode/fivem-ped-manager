628d327b56c8204da2f3825d870140d03365330e
Tom Witkowski <tom.witkowski@frischepost.de>
2022-09-05 12:51:59 +0200

    use vector4

--- client/client.lua
+++ client/client.lua
@@ -7,36 +7,66 @@ end)
 function SpawnPedestrians()
     for _, v in pairs(Config.Peds) do
         local model = GetHashKey(v.ped)
-        RequestModel(model)
-        while not HasModelLoaded(model) do
-            Citizen.Wait(50)
-        end
 
-        local ped = CreatePed(28, model, v.x, v.y, v.z, v.h, false, false)
-        SetEntityHeading(ped, v.h)
-        SetPedCanRagdoll(ped, true)
-        SetEntityInvincible(ped, true)
-        SetEntityProofs(ped, true, true, true, true, true, true, true, true)
-        SetBlockingOfNonTemporaryEvents(ped, true)
-
-        Citizen.Wait(1000)
-
-        if v.task == 'animation' then
+        if TryToRequestModel(model) then
+            local ped = CreatePed(28, model, v.coords.x, v.coords.y, v.coords.z, v.coords.w, false, false)
+            SetEntityHeading(ped, v.coords.w)
             FreezeEntityPosition(ped, true)
+            SetPedCanRagdoll(ped, true)
+            SetEntityInvincible(ped, true)
+            SetEntityProofs(ped, true, true, true, true, true, true, true, true)
+            SetBlockingOfNonTemporaryEvents(ped, true)
 
-            RequestAnimDict(v.animdict)
-            while not HasAnimDictLoaded(v.animdict) do
-                Citizen.Wait(50)
+            Citizen.Wait(1000)
+
+            if v.task == 'animation' then
+                FreezeEntityPosition(ped, true)
+
+                if TryToRequestAnimDict(v.animdict) then
+                    TaskPlayAnim(ped, v.animdict, v.animation, 8.0, 8.0, -1, 9, 1.0, false, false, false)
+                else
+                    DeletePed(ped)
+                end
+            elseif v.task == 'wander' then
+                FreezeEntityPosition(ped, false)
+                TaskWanderInArea(ped, v.coords.x, v.coords.y, v.coords.z, v.wanderradius, v.wanderlength, v.wanderwait)
             end
-
-            TaskPlayAnim(ped, v.animdict, v.animation, 8.0, 8.0, -1, 9, 1.0, false, false, false)
-        end
-
-        if v.task == 'wander' then
-            FreezeEntityPosition(ped, false)
-            TaskWanderInArea(ped, v.x, v.y, v.z, 10.0, 2, 10.0)
         end
 
         SetModelAsNoLongerNeeded(model)
     end
+end
+
+function TryToRequestModel(model)
+    RequestModel(model)
+
+    local i = 0
+    while not HasModelLoaded(model) do
+        if i > 200 then
+            return false
+        end
+
+        Citizen.Wait(50)
+
+        i = i + 1
+    end
+
+    return true
+end
+
+function TryToRequestAnimDict(animdict)
+    RequestAnimDict(animdict)
+
+    local i = 0
+    while not HasAnimDictLoaded(animdict) do
+        if i > 200 then
+            return false
+        end
+
+        Citizen.Wait(50)
+
+        i = i + 1
+    end
+
+    return true
 end
--- config.lua
+++ config.lua
@@ -2,31 +2,78 @@ Config = {}
 
 Config.Peds = {
     {
-        ped = 'a_c_coyote',
-        x = -1205.9636,
-        y = -51.0408,
-        z = 45.5855,
-        h = 318.6644,
-        task = 'animation',
-        animdict = 'creatures@cougar@amb@world_cougar_rest@base',
-        animation = 'armenian_1_int-0',
-    },
-    {
-        animal = 'a_c_mtlion',
-        x = -1163.3102,
-        y = 56.5751,
-        z = 54.3589,
-        h = 272.0989,
+        ped = 's_m_y_waiter_01',
+        coords = vector4(122.0841, -1288.1101, 28.2609, 167.5371),
         task = 'wander',
+        wanderradius = 7.5,
+        wanderlength = 3,
+        wanderwait = 10.0,
     },
+    --[[esx_strippers
     {
         ped = 'a_f_y_topless_01',
-        x = 110.38,
-        y = -1288.34,
-        z = 28.14,
-        heading = 0.0,
+        coords = vector4(110.38, -1288.34, 28.14, 0.0),
         task = 'animation',
         animdict = 'mini@strip_club@lap_dance_2g@ld_2g_p2',
         animation = 'ld_2g_p2_s1',
-    }
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(101.19, -1294.19, 29.4, 251.5),
+        task = 'animation',
+        animdict = 'mini@strip_club@private_dance@part3',
+        animation = 'priv_dance_p3',
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(98.89, -1290.63, 29.4, 30.0),
+        task = 'animation',
+        animdict = 'mini@strip_club@private_dance@part1',
+        animation = 'priv_dance_p1',
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(104.16, -1290.72, 29.0, 308.5),
+        task = 'animation',
+        animdict = 'mini@strip_club@private_dance@part2',
+        animation = 'priv_dance_p2',
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(103.43, -1289.77, 28.38, 29.07),
+        task = 'animation',
+        animdict = 'mini@strip_club@lap_dance@ld_girl_a_song_a_p1',
+        animation = 'ld_girl_a_song_a_p1_f',
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(108.67, -1286.839, 28.49, 308.5),
+        task = 'animation',
+        animdict = 'mini@strip_club@pole_dance@pole_dance3',
+        animation = 'pd_dance_03',
+    },
+    {
+        ped = 'a_f_y_topless_01',
+        coords = vector4(105.31, -1289.87, 28.38, 306.5),
+        task = 'animation',
+        animdict = 'mini@strip_club@lap_dance_2g@ld_2g_p1',
+        animation = 'ld_2g_p1_s1',
+    },
+    ]]
+    --[[Peds
+    {
+        ped = 's_m_m_chemsec_01',
+        coords = vector4(123.57, -1290.38, 28.29, 240.82),
+        task = 'animation',
+        animdict = 'anim@amb@nightclub@peds@',
+        animation = 'amb_world_human_stand_guard_male_base',
+    },
+    {
+        ped = 's_m_m_chemsec_01',
+        coords = vector4(107.82, -1298.90, 27.77, 172.95),
+        task = 'animation',
+        animdict = 'anim@amb@nightclub@peds@',
+        animation = 'amb_world_human_stand_guard_male_base',
+    },
+    ]]
 }
